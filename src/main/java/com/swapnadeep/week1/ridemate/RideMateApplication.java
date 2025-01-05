3b8995f2f3a86671647dcaa91e4ef78397092da7
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2025-01-05 23:28:30 +0530

    init: project

---
+++ src/main/java/com/swapnadeep/week1/ridemate/RideMateApplication.java
@@ -0,0 +1,13 @@
+package com.swapnadeep.week1.ridemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swapnadeep/week1/ridemate/RideMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnadeep.week1.ridemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
