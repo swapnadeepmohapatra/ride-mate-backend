10b48dd6a88c179081ca52b8dc100cabb3aafdf0
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2025-01-08 10:28:46 +0530

    add: user entity

---
+++ src/main/java/com/swapnadeep/ridemate/entities/User.java
@@ -0,0 +1,28 @@
+package com.swapnadeep.ridemate.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users", indexes = {
+        @Index(name = "idx_user_email", columnList = "email")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
